3ad953f9e57e3df986d627d94b0b48edf356d4f1
Mohammed Eldabbagh <maldabbagh.c@t4edu.com>
2021-08-15 14:21:10 +0300

    Init.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Graph;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using UploadingLagreFiles_JavaScriptFileSplit.Models;
+
+namespace UploadingLagreFiles_JavaScriptFileSplit.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private IHostingEnvironment Environment;
+
+        public HomeController(ILogger<HomeController> logger, IHostingEnvironment _environment)
+        {
+            _logger = logger;
+            Environment = _environment;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+      
+        private async Task<byte[]> GetByteArrayFromImageAsync(IFormFile file)
+        {
+            using (var target = new MemoryStream())
+            {
+                await file.CopyToAsync(target);
+                return target.ToArray();
+            }
+        }
+
+
+        [HttpPost]
+        public IActionResult UploadFile(IFormFile files)
+        {
+          
+            var FileDataContent = Request.Form.Files[0];
+            if (FileDataContent != null && FileDataContent.Length > 0)
+            {
+                // take the input stream, and save it to a temp folder using  
+                // the original file.part name posted  
+                var stream = FileDataContent.OpenReadStream();
+                var fileName = Path.GetFileName(FileDataContent.FileName);
+                var tempFolderName = "yourFolderThatContainSpiltFiles";
+                var UploadPath = this.Environment.WebRootPath + "/temp/files/" + tempFolderName;
+             
+                System.IO.Directory.CreateDirectory(UploadPath);
+                string path = Path.Combine(UploadPath, fileName);
+                try
+                {
+                    if (System.IO.File.Exists(path))
+                        System.IO.File.Delete(path);
+                    using (var fileStream = System.IO.File.Create(path))
+                    {
+                        stream.CopyTo(fileStream);
+                    }
+                    // Once the file part is saved, see if we have enough to merge it  
+                    //UploadingLagreFiles_JavaScriptFileSplit.Models.Shared.Utils UT = new UploadingLagreFiles_JavaScriptFileSplit.Models.Shared.Utils();
+                    //UT.MergeFile(path);
+                }
+                catch (IOException ex)
+                {
+                    // handle  
+                }
+                return new JsonResult(new { scusses = true, fileName = FileDataContent.FileName, folderPath = UploadPath });
+
+            }
+            return new JsonResult(new { scusses = false });
+
+
+        }
+
+
+        [HttpPost]
+        public IActionResult MergeFiles(List<string> files)
+        {
+            CombineMultipleFilesIntoSingleFile(this.Environment.WebRootPath + "/temp/files/yourFolderThatContainSpiltFiles",
+                this.Environment.WebRootPath + "/temp/files/yourFolderThatContainSpiltFiles/MergedFile.zip");
+            return new JsonResult(new { scusses = true });
+        }
+
+        private static void CombineMultipleFilesIntoSingleFile(string inputDirectoryPath, string outputFilePath)
+        {
+            string[] inputFilePaths = System.IO.Directory.GetFiles(inputDirectoryPath);
+            Console.WriteLine("Number of files: {0}.", inputFilePaths.Length);
+            using (var outputStream =System.IO.File.Create(outputFilePath))
+            {
+                foreach (var inputFilePath in inputFilePaths)
+                {
+                    using (var inputStream = System.IO.File.OpenRead(inputFilePath))
+                    {
+                        // Buffer size can be passed as the second argument.
+                        inputStream.CopyTo(outputStream);
+                    }
+                    Console.WriteLine("The file {0} has been processed.", inputFilePath);
+                }
+            }
+        }
+
+
+    }
+}
---
+++ Controllers/UploadController.cs
@@ -0,0 +1,224 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Graph;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using UploadingLagreFiles_JavaScriptFileSplit.Models;
+
+namespace UploadingLagreFiles_JavaScriptFileSplit.Controllers
+{
+	public class UploadViewRequest
+	{
+		public Guid UploadFolder { get; set; }
+		public bool IsChunk { get; set; }
+		public int ChunkNumber { get; set; }
+		public bool IsFirst { get; set; }
+		public bool IsLast { get; set; }
+		public IFormFile OriginalFile { get; set; }
+		public bool JsonAccepted { get; set; }
+	}
+
+	public class FileStatus
+	{
+		public const string HandlerPath = "/";
+
+		public string group { get; set; }
+		public string name { get; set; }
+		public string type { get; set; }
+		public int size { get; set; }
+		public string progress { get; set; }
+		public string url { get; set; }
+		public string thumbnail_url { get; set; }
+		public string delete_url { get; set; }
+		public string delete_type { get; set; }
+		public string error { get; set; }
+
+		public FileStatus()
+		{
+		}
+
+		public FileStatus(FileInfo fileInfo)
+		{
+			SetValues(fileInfo.Name, (int)fileInfo.Length, fileInfo.FullName);
+		}
+
+		public FileStatus(string fileName, int fileLength, string fullPath)
+		{
+			SetValues(fileName, fileLength, fullPath);
+		}
+
+		private void SetValues(string fileName, int fileLength, string fullPath)
+		{
+			type = null;
+			name = fileName;
+			url = null;
+			size = fileLength;
+			progress = "1.0";
+			delete_url = null;
+			delete_type = "DELETE";
+			thumbnail_url = null;
+		}
+	}
+
+	public class UploadController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+		private IHostingEnvironment Environment;
+
+		public UploadController(ILogger<HomeController> logger, IHostingEnvironment _environment)
+		{
+			_logger = logger;
+			Environment = _environment;
+		}
+
+		[HttpGet]
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Index(UploadViewRequest model)
+		{
+			FileStatus status = null;
+			try
+			{
+				model ??= new UploadViewRequest();
+
+				model.OriginalFile = Request.Form.Files.FirstOrDefault();
+
+				var rangeHeader = Request.Headers["Content-Range"];
+				if (string.IsNullOrEmpty(rangeHeader))
+				{
+					model.IsChunk = false;
+				}
+				else
+				{
+					model.IsChunk = true;
+
+					Match match = Regex.Match(rangeHeader, "^bytes ([\\d]+)-([\\d]+)\\/([\\d]+)$", RegexOptions.IgnoreCase);
+					int bytesFrom = int.Parse(match.Groups[1].Value);
+					int bytesTo = int.Parse(match.Groups[2].Value);
+					int bytesFull = int.Parse(match.Groups[3].Value);
+
+					if (bytesTo == bytesFull)
+						model.IsLast = true;
+					else
+						model.IsLast = false;
+
+					if (bytesFrom == 0)
+					{
+						model.ChunkNumber = 1;
+						model.IsFirst = true;
+					}
+					else
+					{
+						int bytesSize = bytesTo - bytesFrom + 1;
+						model.ChunkNumber = (bytesFrom / bytesSize) + 1;
+						model.IsFirst = false;
+					}
+				}
+
+				if (Request.Headers.ContainsKey("Accept") && Request.Headers["Accept"].ToString().Contains("application/json"))
+				{
+					model.JsonAccepted = true;
+				}
+				else
+				{
+					model.JsonAccepted = false;
+				}
+
+				// Section 2
+				var fileName = model.OriginalFile.FileName;
+				var path = this.Environment.WebRootPath + "/storage/temp/" + fileName;
+				
+				if (model.IsChunk)
+				{
+					if (model.IsFirst)
+					{
+						// do some stuff that has to be done before the file starts uploading
+						if (System.IO.File.Exists(path))
+						{
+							System.IO.File.Delete(path);
+						}
+					}
+
+					var inputStream = model.OriginalFile.OpenReadStream();
+
+					using (var fs = new FileStream(path, FileMode.Append, FileAccess.Write))
+					{
+						var buffer = new byte[1024];
+
+						var l = inputStream.Read(buffer, 0, 1024);
+						while (l > 0)
+						{
+							fs.Write(buffer, 0, l);
+							l = inputStream.Read(buffer, 0, 1024);
+						}
+
+						fs.Flush();
+						fs.Close();
+					}
+
+					status = new FileStatus(new FileInfo(path));
+
+					if (model.IsLast)
+					{
+						// do some stuff that has to be done after the file is uploaded
+					}
+				}
+				else
+				{
+					if (System.IO.File.Exists(path))
+					{
+						System.IO.File.Delete(path);
+					}
+					using (Stream fileStream = new FileStream(path, FileMode.Create))
+					{
+						model.OriginalFile.CopyTo(fileStream);
+					}
+					status = new FileStatus(new FileInfo(path));
+				}
+			}
+			catch (Exception ex)
+			{
+				status = new FileStatus
+				{
+					error = "Something went wrong"
+				};
+			}
+
+			// this is just a browser json support/compatibility workaround
+			if (model.JsonAccepted)
+			{
+				return Json(status);
+			}
+			else
+			{
+				return Json(status, "text/plain");
+			}
+		}
+
+		[HttpHead]
+		public IActionResult Index(string filename)
+		{
+			var path = this.Environment.WebRootPath + "/storage/temp/" + filename;
+			if (System.IO.File.Exists(path))
+			{
+				return Json(new { status = true, fileSize = new FileInfo(path).Length });
+			}
+			else
+			{
+				return Json(new { status = false });
+			}
+		}
+	}
+}
